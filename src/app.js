7096c4cc92b179b176ca23df24d5901f6a0f92c9
Sina-Afshar <hawking0909@gmail.com>
2021-10-19 13:54:38 +0300

    18 ekim

---
+++ src/app.js
@@ -0,0 +1,18 @@
+console.log("Merhaba kodlama io.")
+
+let konutKredileri = ["Konut kredisi", "Emlak konut kredisi", "Kamu konut kredisi",]
+
+console.log("<ul>")
+for (let i = 0; i < konutKredileri.length; i++) {
+    console.log("<li>"+konutKredileri[i]+"</li>")
+
+}
+console.log("</ul>")
+
+
+
+
+// test
+// let, const : let degeri degistirilebilir : const degeri degistirilemez
+// let konutKredisi = ["Array / diziler : dizileri listeler metinsel ve ya sayisal"]
+//camelCasing ve ya PascalCasing 
